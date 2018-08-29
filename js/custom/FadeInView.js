5948ff03a9c33865c8ee44893db0bddbe9b6baf3
韩承海 <374802597@qq.com>
2018-08-29 10:37:33 +0800

    no message

--- js/custom/FadeInView.js
+++ js/custom/FadeInView.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
-import {Animated, Easing, Text} from 'react-native';
+import React, { Component } from 'react';
+import { Animated, Easing, Text } from 'react-native';
 
 export default class FadeInView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fadeAnim: new Animated.Value(0)
-        };
-    }
-    componentDidMount() {
-        Animated
-            .timing(this.state.fadeAnim, {
-            toValue: 1,
-            easing: Easing.linear,
-            duration: 5000,
-        })
-            .start();
-    }
-    componentWillReceiveProps() {
-        
-    }
-    render() {
-        return (
-            <Animated.View
-                style={{
-                ...this.props.style,
-                opacity: this.state.fadeAnim
-            }}>
-                {this.props.children}
-            </Animated.View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeAnim: new Animated.Value(0)
+    };
+  }
+  componentDidMount() {
+    Animated.timing(this.state.fadeAnim, {
+      toValue: 1,
+      easing: Easing.linear,
+      duration: 5000
+    }).start();
+  }
+  componentWillReceiveProps() {}
+  render() {
+    return (
+      <Animated.View
+        style={{
+          //props提供的语法糖，可以将父组件中的所有属性复制给子组件。
+          ...this.props.style,
+          opacity: this.state.fadeAnim
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
 }
