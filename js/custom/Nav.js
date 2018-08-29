6205f1b3c89b4b13a25f587fe713827a8a02ff41
韩承海 <374802597@qq.com>
2018-08-29 18:07:08 +0800

    no message

--- js/custom/Nav.js
+++ js/custom/Nav.js
@@ -1,30 +1,32 @@
 import React, { Component, PropTypes } from 'react';
-import {NavgatorIOS, Text, TouchableHignlight, View} from 'react-native';
+import { NavgatorIOS, Text, TouchableHignlight, View } from 'react-native';
 
 class MyScene extends Component {
-    static propTypes = {
-        title: PropTypes.string,isRequired,
-        navigator: PropTypes.object.isRequired,
-    }
-    constructor(props, context) {
-        super(props, context);
-        this._onFprword = this._onFprword.bind(this);
-    }
+  static propTypes = {
+    title: PropTypes.string,
+    isRequired,
+    navigator: PropTypes.object.isRequired
+  };
+  constructor(props, context) {
+    super(props, context);
+    //需要在自定义函数中使用this 所以要bind
+    this._onFprword = this._onFprword.bind(this);
+  }
 
-    _onFprword() {
-        this.props.navigator.push({
-            title: 'Scene' + nextIndex,
-        });
-    }
+  _onFprword() {
+    this.props.navigator.push({
+      title: 'Scene' + nextIndex
+    });
+  }
 
-    render() {
-        return (
-            <View>
-                <Text>Current Scene: {this.props.title}</Text>
-                <TouchableHignlight onPress={this._onFprword}>
-                    <Text>Tap me to load the next scene</Text>
-                </TouchableHignlight>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: {this.props.title}</Text>
+        <TouchableHignlight onPress={this._onFprword}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHignlight>
+      </View>
+    );
+  }
 }
